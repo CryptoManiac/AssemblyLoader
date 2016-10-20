debe4723a42d51d858bb8c422ad9cf8ded048021
CryptoManiac <balthazar@yandex.ru>
2016-10-20 13:26:52 +0300

    Define MethodAttribute, FIeldAttribute and MethodImplAttributes enums

--- PicoVM/CLR/CLIMetaTableRows.hxx
+++ PicoVM/CLR/CLIMetaTableRows.hxx
@@ -33,77 +33,45 @@ struct TypeDefRow {
     enum TypeAttributes : uint32_t {
         // Use this mask to retrieve the type visibility information.
         VisibilityMask        =   0x00000007,
-        // Class is not public scope.
-        NotPublic             =   0x00000000,     
-        // Class is public scope.
-        Public                =   0x00000001,     
-        // Class is nested with public visibility.
-        NestedPublic          =   0x00000002,     
-        // Class is nested with private visibility.
-        NestedPrivate         =   0x00000003,     
-        // Class is nested with family visibility.
-        NestedFamily          =   0x00000004,     
-        // Class is nested with assembly visibility.
-        NestedAssembly        =   0x00000005,     
-        // Class is nested with family and assembly visibility.
-        NestedFamANDAssem     =   0x00000006,     
-        // Class is nested with family or assembly visibility.
-        NestedFamORAssem      =   0x00000007,     
+        NotPublic             =   0x00000000, // Class is not public scope.
+        Public                =   0x00000001, // Class is public scope.
+        NestedPublic          =   0x00000002, // Class is nested with public visibility.
+        NestedPrivate         =   0x00000003, // Class is nested with private visibility.
+        NestedFamily          =   0x00000004, // Class is nested with family visibility.
+        NestedAssembly        =   0x00000005, // Class is nested with assembly visibility.
+        NestedFamANDAssem     =   0x00000006, // Class is nested with family and assembly visibility.
+        NestedFamORAssem      =   0x00000007, // Class is nested with family or assembly visibility.
 
-        // Use this mask to retrieve class layout information
-        LayoutMask            =   0x00000018,
-        // Class fields are auto-laid out
-        AutoLayout            =   0x00000000,     
-        // Class fields are laid out sequentially
-        SequentialLayout      =   0x00000008,     
-        // Layout is supplied explicitly
-        ExplicitLayout        =   0x00000010,     
-
-        // Use this mask to retrieve class semantics information.
-        ClassSemanticsMask    =   0x00000060,
-        // Type is a class.
-        Class                 =   0x00000000,     
-        // Type is an interface.
-        Interface             =   0x00000020,     
+        // Use this mask to retrieve class layout information.
+        LayoutMask            =   0x00000018, 
+        AutoLayout            =   0x00000000, // Class fields are auto-laid out.
+        SequentialLayout      =   0x00000008, // Class fields are laid out sequentially.
+        ExplicitLayout        =   0x00000010, // Layout is supplied explicitly.
+        ClassSemanticsMask    =   0x00000060, // Use this mask to retrieve class semantics information.
+        Class                 =   0x00000000, // Type is a class.
+        Interface             =   0x00000020, // Type is an interface.
 
         // Special semantics in addition to class semantics.
+        Abstract              =   0x00000080, // Class is abstract.
+        Sealed                =   0x00000100, // Class is concrete and may not be extended.     
+        SpecialName           =   0x00000400, // Class name is special. Name describes how.
+        Import                =   0x00001000, // Class / interface is imported.
+        Serializable          =   0x00002000, // The class is Serializable.
 
-        // Class is abstract
-        Abstract              =   0x00000080,     
-        // Class is concrete and may not be extended
-        Sealed                =   0x00000100,     
-        // Class name is special. Name describes how.
-        SpecialName           =   0x00000400,
-
-        // Class / interface is imported
-        Import                =   0x00001000,     
-        // The class is Serializable.
-        Serializable          =   0x00002000,     
-
-        // Use tdStringFormatMask to retrieve string information for native interop
+        // Use to retrieve string information for native interop.
         StringFormatMask      =   0x00030000,
-        // LPTSTR is interpreted as ANSI in this class
-        AnsiClass             =   0x00000000,     
-        // LPTSTR is interpreted as UNICODE
-        UnicodeClass          =   0x00010000,     
-        // LPTSTR is interpreted automatically
-        AutoClass             =   0x00020000,     
-        // A non-standard encoding specified by CustomFormatMask
-        CustomFormatClass     =   0x00030000,     
+        AnsiClass             =   0x00000000, // LPTSTR is interpreted as ANSI in this class.
+        UnicodeClass          =   0x00010000, // LPTSTR is interpreted as UNICODE.
+        AutoClass             =   0x00020000, // LPTSTR is interpreted automatically.
+        CustomFormatClass     =   0x00030000, // A non-standard encoding specified by CustomFormatMask.
 
-        // Use this mask to retrieve non-standard encoding information for native interop. 
-        // The meaning of the values of these 2 bits is unspecified.
-        CustomFormatMask      =   0x00C00000,
-        // Initialize the class any time before first static field access.
-        BeforeFieldInit       =   0x00100000,     
-        // This ExportedType is a type forwarder.
-        Forwarder             =   0x00200000,     
-        // Flags reserved for runtime use.
-        ReservedMask          =   0x00040800,
-        // Runtime should check name encoding.
-        RTSpecialName         =   0x00000800,     
-        // Class has security associate with it.
-        HasSecurity           =   0x00040000,
+        // Use this mask to retrieve non-standard encoding information for native interop.
+        CustomFormatMask      =   0x00C00000, // The meaning of the values of these 2 bits is unspecified.
+        BeforeFieldInit       =   0x00100000, // Initialize the class any time before first static field access.
+        Forwarder             =   0x00200000, // This ExportedType is a type forwarder.
+        ReservedMask          =   0x00040800, // Flags reserved for runtime use.
+        RTSpecialName         =   0x00000800, // Runtime should check name encoding.
+        HasSecurity           =   0x00040000, // Class has security associate with it.
     };
 };
 
@@ -112,6 +80,33 @@ struct FieldRow {
     uint16_t flags;
     std::vector<uint16_t> name;
     std::vector<uint32_t> signature;
+
+    enum FieldAttributes : uint16_t {
+        // Use this mask to retrieve accessibility information.
+        FieldAccessMask           =   0x0007,
+        PrivateScope              =   0x0000, // Member not referenceable.
+        Private                   =   0x0001, // Accessible only by the parent type.
+        FamANDAssem               =   0x0002, // Accessible by sub-types only in this Assembly.
+        Assembly                  =   0x0003, // Accessibly by anyone in the Assembly.
+        Family                    =   0x0004, // Accessible only by type and sub-types.
+        FamORAssem                =   0x0005, // Accessibly by sub-types anywhere, plus anyone in assembly.
+        Public                    =   0x0006, // Accessibly by anyone who has visibility to this scope.
+
+        // Field contract attributes.
+        Static                    =   0x0010, // Defined on type, else per instance.
+        InitOnly                  =   0x0020, // Field may only be initialized, not written to after init.
+        Literal                   =   0x0040, // Value is compile time constant.
+        NotSerialized             =   0x0080, // Field does not have to be serialized when type is remoted.
+        SpecialName               =   0x0200, // Field is special. Name describes how.
+
+        // Interop attributes
+        PinvokeImpl               =   0x2000, // Implementation is forwarded through pinvoke.
+        ReservedMask              =   0x9500, // Reserved flags for runtime use only.
+        RTSpecialName             =   0x0400, // Runtime(metadata internal APIs) should check name encoding.
+        HasFieldMarshal           =   0x1000, // Field has marshalling information.
+        HasDefault                =   0x8000, // Field has default.
+        HasFieldRVA               =   0x0100, // Field has RVA.
+    };
 };
 
 struct MethodDefRow {
@@ -123,6 +118,69 @@ struct MethodDefRow {
     std::vector<uint16_t> name;
     std::vector<uint32_t> signature;
     uint32_t paramList;
+
+    enum MethodAttribute {
+        // Use this mask to retrieve accessibility information.
+        mdMemberAccessMask          =   0x0007,
+        mdPrivateScope              =   0x0000, // Member not referenceable.
+        mdPrivate                   =   0x0001, // Accessible only by the parent type.     
+        mdFamANDAssem               =   0x0002, // Accessible by sub-types only in this Assembly.     
+        mdAssem                     =   0x0003, // Accessibly by anyone in the Assembly.     
+        mdFamily                    =   0x0004, // Accessible only by type and sub-types.     
+        mdFamORAssem                =   0x0005, // Accessibly by sub-types anywhere, plus anyone in assembly.     
+        mdPublic                    =   0x0006, // Accessibly by anyone who has visibility to this scope.     
+
+        // method contract attributes.
+        mdStatic                    =   0x0010, // Defined on type, else per instance.     
+        mdFinal                     =   0x0020, // Method may not be overridden.     
+        mdVirtual                   =   0x0040, // Method virtual.     
+        mdHideBySig                 =   0x0080, // Method hides by name+sig, else just by name.
+
+        // Use this mask to retrieve vtable attributes.
+        mdVtableLayoutMask          =   0x0100,
+        mdReuseSlot                 =   0x0000, // The default.
+        mdNewSlot                   =   0x0100, // Method always gets a new slot in the vtable.
+
+        // Method implementation attributes.
+        mdCheckAccessOnOverride     =   0x0200, // Overridability is the same as the visibility.
+        mdAbstract                  =   0x0400, // Method does not provide an implementation.   
+        mdSpecialName               =   0x0800, // Method is special. Name describes how.    
+       
+        // Interop attributes
+        mdPinvokeImpl               =   0x2000, // Implementation is forwarded through pinvoke.
+        mdUnmanagedExport           =   0x0008, // Managed method exported via thunk to unmanaged code.
+       
+        // Reserved flags for runtime use only.
+        mdReservedMask              =   0xd000,
+        mdRTSpecialName             =   0x1000, // Runtime should check name encoding.
+        mdHasSecurity               =   0x4000, // Method has security associate with it.
+        mdRequireSecObject          =   0x8000, // Method calls another method containing security code.
+       
+
+
+    };
+
+    enum MethodImplAttributes {
+        // Code impl mask
+        miCodeTypeMask      =   0x0003,   // Flags about code type.
+        miIL                =   0x0000,   // Method impl is IL.
+        miNative            =   0x0001,   // Method impl is native.
+        miOPTIL             =   0x0002,   // Method impl is OPTIL
+        miRuntime           =   0x0003,   // Method impl is provided by the runtime.
+
+        // Managed mask
+        miManagedMask       =   0x0004,   // Flags specifying whether the code is managed or unmanaged.
+        miUnmanaged         =   0x0004,   // Method impl is unmanaged, otherwise managed.
+        miManaged           =   0x0000,   // Method impl is managed.
+
+        // Implementation info and interop
+        miForwardRef        =   0x0010,   // Indicates method is defined; used primarily in merge scenarios.
+        miPreserveSig       =   0x0080,   // Indicates method sig is not to be mangled to do HRESULT conversion.
+        miInternalCall      =   0x1000,   // Reserved for internal use.
+        miSynchronized      =   0x0020,   // Method is single threaded through the body.
+        miNoInlining        =   0x0008,   // Method may not be inlined.
+        miMaxMethodImplVal  =   0xffff,   // Range check value
+    };
 };
 
 struct ParamRow {
