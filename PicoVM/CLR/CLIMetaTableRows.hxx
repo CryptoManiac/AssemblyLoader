0e74d78f7a9c607a9b9d507bb560a9f8d7a1cd67
CryptoManiac <balthazar.ad@gmail.com>
2016-10-20 20:05:40 +0300

    Fix compilation error

--- PicoVM/CLR/CLIMetaTableRows.hxx
+++ PicoVM/CLR/CLIMetaTableRows.hxx
@@ -475,19 +475,19 @@ struct GenericParamRow {
     enum GenericParamAttributes {
         // Variance of type parameters, only applicable to generic parameters 
         // for generic interfaces and delegates
-        gpVarianceMask          =   0x0003,
-        gpNonVariant            =   0x0000, 
-        gpCovariant             =   0x0001,
-        gpContravariant         =   0x0002,
+        gpVarianceMask = 0x0003,
+        gpNonVariant = 0x0000,
+        gpCovariant = 0x0001,
+        gpContravariant = 0x0002,
 
         // Special constraints, applicable to any type parameters
-        gpSpecialConstraintMask =  0x001C,
-        gpNoSpecialConstraint   =   0x0000,      
+        gpSpecialConstraintMask = 0x001C,
+        gpNoSpecialConstraint = 0x0000,
         gpReferenceTypeConstraint = 0x0004, // type argument must be a reference type
         gpNotNullableValueTypeConstraint = 0x0008, // type argument must be a value type but not Nullable
         gpDefaultConstructorConstraint = 0x0010, // type argument must have a public
                                                 // default constructor
-    }
+    };
 };
 
 struct MethodSpecRow {
