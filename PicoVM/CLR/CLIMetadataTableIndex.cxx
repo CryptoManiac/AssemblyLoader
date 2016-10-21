b4de08293134a7d1da48856cbf0981b7f9321252
CryptoManiac <balthazar.ad@gmail.com>
2016-10-21 23:43:34 +0300

    Alias for better readability.

--- PicoVM/CLR/CLIMetadataTableIndex.cxx
+++ PicoVM/CLR/CLIMetadataTableIndex.cxx
@@ -7,72 +7,74 @@ using namespace std;
 
 CLIMetadataTableIndex& operator++(CLIMetadataTableIndex& value)
 {
-    const vector<CLIMetadataTableIndex> tables = {
-        /* 00 */ CLIMetadataTableIndex::Module,                 /* 01 */ CLIMetadataTableIndex::TypeRef,                /* 02  */ CLIMetadataTableIndex::TypeDef,
-        /* 04 */ CLIMetadataTableIndex::Field,                  /* 06 */ CLIMetadataTableIndex::MethodDef,              /* 08  */ CLIMetadataTableIndex::Param,
-        /* 09 */ CLIMetadataTableIndex::InterfaceImpl,          /* 10 */ CLIMetadataTableIndex::MemberRef,              /* 11  */ CLIMetadataTableIndex::Constant,
-        /* 12 */ CLIMetadataTableIndex::CustomAttribute,        /* 13 */ CLIMetadataTableIndex::FieldMarshal,           /* 14  */ CLIMetadataTableIndex::DeclSecurity,
-        /* 15 */ CLIMetadataTableIndex::ClassLayout,            /* 16 */ CLIMetadataTableIndex::FieldLayout,            /* 17  */ CLIMetadataTableIndex::StandAloneSig,
-        /* 18 */ CLIMetadataTableIndex::EventMap,               /* 20 */ CLIMetadataTableIndex::Event,                  /* 21  */ CLIMetadataTableIndex::PropertyMap,
-        /* 23 */ CLIMetadataTableIndex::Property,               /* 24 */ CLIMetadataTableIndex::MethodSemantics,        /* 25  */ CLIMetadataTableIndex::MethodImpl,
-        /* 26 */ CLIMetadataTableIndex::ModuleRef,              /* 27 */ CLIMetadataTableIndex::TypeSpec,               /* 28  */ CLIMetadataTableIndex::ImplMap,
-        /* 29 */ CLIMetadataTableIndex::FieldRVA,               /* 32 */ CLIMetadataTableIndex::Assembly,               /* 33  */ CLIMetadataTableIndex::AssemblyProcessor,
-        /* 34 */ CLIMetadataTableIndex::AssemblyOS,             /* 35 */ CLIMetadataTableIndex::AssemblyRef,            /* 36  */ CLIMetadataTableIndex::AssemblyRefProcessor,
-        /* 37 */ CLIMetadataTableIndex::AssemblyRefOS,          /* 38 */ CLIMetadataTableIndex::File,                   /* 39  */ CLIMetadataTableIndex::ExportedType,
-        /* 40 */ CLIMetadataTableIndex::ManifestResource,       /* 41 */ CLIMetadataTableIndex::NestedClass,            /* 42  */ CLIMetadataTableIndex::GenericParam,
-        /* 43 */ CLIMetadataTableIndex::MethodSpec,             /* 44 */ CLIMetadataTableIndex::GenericParamConstraint, /* 256 */ CLIMetadataTableIndex::Unknown
+    using idx = CLIMetadataTableIndex;
+    const vector<idx> tables = {
+        /* 00 */ idx::Module,                 /* 01 */ idx::TypeRef,                /* 02  */ idx::TypeDef,
+        /* 04 */ idx::Field,                  /* 06 */ idx::MethodDef,              /* 08  */ idx::Param,
+        /* 09 */ idx::InterfaceImpl,          /* 10 */ idx::MemberRef,              /* 11  */ idx::Constant,
+        /* 12 */ idx::CustomAttribute,        /* 13 */ idx::FieldMarshal,           /* 14  */ idx::DeclSecurity,
+        /* 15 */ idx::ClassLayout,            /* 16 */ idx::FieldLayout,            /* 17  */ idx::StandAloneSig,
+        /* 18 */ idx::EventMap,               /* 20 */ idx::Event,                  /* 21  */ idx::PropertyMap,
+        /* 23 */ idx::Property,               /* 24 */ idx::MethodSemantics,        /* 25  */ idx::MethodImpl,
+        /* 26 */ idx::ModuleRef,              /* 27 */ idx::TypeSpec,               /* 28  */ idx::ImplMap,
+        /* 29 */ idx::FieldRVA,               /* 32 */ idx::Assembly,               /* 33  */ idx::AssemblyProcessor,
+        /* 34 */ idx::AssemblyOS,             /* 35 */ idx::AssemblyRef,            /* 36  */ idx::AssemblyRefProcessor,
+        /* 37 */ idx::AssemblyRefOS,          /* 38 */ idx::File,                   /* 39  */ idx::ExportedType,
+        /* 40 */ idx::ManifestResource,       /* 41 */ idx::NestedClass,            /* 42  */ idx::GenericParam,
+        /* 43 */ idx::MethodSpec,             /* 44 */ idx::GenericParamConstraint, /* 256 */ idx::Unknown
     };
 
     auto it = find(tables.cbegin(), tables.cend(), value);
-    if (*it == CLIMetadataTableIndex::Unknown || it == tables.cend()) {
+    if (*it == idx::Unknown || it == tables.cend()) {
         // Overflow
-        return value = CLIMetadataTableIndex::Module;
+        return value = idx::Module;
     }
 
     return value = *next(it, 1);
 }
 
 string getTableName(const CLIMetadataTableIndex& value) {
-    const map<CLIMetadataTableIndex, string> tables = {
-        { CLIMetadataTableIndex::Module, "Module"},
-        { CLIMetadataTableIndex::TypeRef, "TypeRef"},
-        { CLIMetadataTableIndex::TypeDef, "TypeDef"},
-        { CLIMetadataTableIndex::Field, "Field"},
-        { CLIMetadataTableIndex::MethodDef, "MethodDef"},
-        { CLIMetadataTableIndex::Param, "Param"},
-        { CLIMetadataTableIndex::InterfaceImpl, "InterfaceImpl"},
-        { CLIMetadataTableIndex::MemberRef, "MemberRef"},
-        { CLIMetadataTableIndex::Constant, "Constant"},
-        { CLIMetadataTableIndex::CustomAttribute, "CustomAttribute"},
-        { CLIMetadataTableIndex::FieldMarshal, "FieldMarshal"},
-        { CLIMetadataTableIndex::DeclSecurity, "DeclSecurity"},
-        { CLIMetadataTableIndex::ClassLayout, "ClassLayout"},
-        { CLIMetadataTableIndex::FieldLayout, "FieldLayout"},
-        { CLIMetadataTableIndex::StandAloneSig, "StandAloneSig"},
-        { CLIMetadataTableIndex::EventMap, "EventMap"},
-        { CLIMetadataTableIndex::Event, "Event"},
-        { CLIMetadataTableIndex::PropertyMap, "PropertyMap"},
-        { CLIMetadataTableIndex::Property, "Property"},
-        { CLIMetadataTableIndex::MethodSemantics, "MethodSemantics"},
-        { CLIMetadataTableIndex::MethodImpl, "MethodImpl"},
-        { CLIMetadataTableIndex::ModuleRef, "ModuleRef"},
-        { CLIMetadataTableIndex::TypeSpec, "TypeSpec"},
-        { CLIMetadataTableIndex::ImplMap, "ImplMap"},
-        { CLIMetadataTableIndex::FieldRVA, "FieldRVA"},
-        { CLIMetadataTableIndex::Assembly, "Assembly"},
-        { CLIMetadataTableIndex::AssemblyProcessor, "AssemblyProcessor"},
-        { CLIMetadataTableIndex::AssemblyOS, "AssemblyOS"},
-        { CLIMetadataTableIndex::AssemblyRef, "AssemblyRef"},
-        { CLIMetadataTableIndex::AssemblyRefProcessor, "AssemblyRefProcessor"},
-        { CLIMetadataTableIndex::AssemblyRefOS, "AssemblyRefOS"},
-        { CLIMetadataTableIndex::File, "File"},
-        { CLIMetadataTableIndex::ExportedType, "ExportedType"},
-        { CLIMetadataTableIndex::ManifestResource, "ManifestResource"},
-        { CLIMetadataTableIndex::NestedClass, "NestedClass"},
-        { CLIMetadataTableIndex::GenericParam, "GenericParam"},
-        { CLIMetadataTableIndex::MethodSpec, "MethodSpec"},
-        { CLIMetadataTableIndex::GenericParamConstraint, "GenericParamConstraint" },
-        { CLIMetadataTableIndex::Unknown, "Unknown"}
+    using idx = CLIMetadataTableIndex;
+    const map<idx, string> tables = {
+        { idx::Module, "Module"},
+        { idx::TypeRef, "TypeRef"},
+        { idx::TypeDef, "TypeDef"},
+        { idx::Field, "Field"},
+        { idx::MethodDef, "MethodDef"},
+        { idx::Param, "Param"},
+        { idx::InterfaceImpl, "InterfaceImpl"},
+        { idx::MemberRef, "MemberRef"},
+        { idx::Constant, "Constant"},
+        { idx::CustomAttribute, "CustomAttribute"},
+        { idx::FieldMarshal, "FieldMarshal"},
+        { idx::DeclSecurity, "DeclSecurity"},
+        { idx::ClassLayout, "ClassLayout"},
+        { idx::FieldLayout, "FieldLayout"},
+        { idx::StandAloneSig, "StandAloneSig"},
+        { idx::EventMap, "EventMap"},
+        { idx::Event, "Event"},
+        { idx::PropertyMap, "PropertyMap"},
+        { idx::Property, "Property"},
+        { idx::MethodSemantics, "MethodSemantics"},
+        { idx::MethodImpl, "MethodImpl"},
+        { idx::ModuleRef, "ModuleRef"},
+        { idx::TypeSpec, "TypeSpec"},
+        { idx::ImplMap, "ImplMap"},
+        { idx::FieldRVA, "FieldRVA"},
+        { idx::Assembly, "Assembly"},
+        { idx::AssemblyProcessor, "AssemblyProcessor"},
+        { idx::AssemblyOS, "AssemblyOS"},
+        { idx::AssemblyRef, "AssemblyRef"},
+        { idx::AssemblyRefProcessor, "AssemblyRefProcessor"},
+        { idx::AssemblyRefOS, "AssemblyRefOS"},
+        { idx::File, "File"},
+        { idx::ExportedType, "ExportedType"},
+        { idx::ManifestResource, "ManifestResource"},
+        { idx::NestedClass, "NestedClass"},
+        { idx::GenericParam, "GenericParam"},
+        { idx::MethodSpec, "MethodSpec"},
+        { idx::GenericParamConstraint, "GenericParamConstraint" },
+        { idx::Unknown, "Unknown"}
     };
 
     auto it = tables.find(value);
